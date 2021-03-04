64e9db625f042a053cbbb6beae19ad6f31de58d7
redbird <redbird8941@gmail.com>
2021-03-05 01:09:47 +0300

    shops-service initialized

---
+++ src/main/java/com/redbird/shopsservice/ShopsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.redbird.shopsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redbird/shopsservice/ShopsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbird.shopsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
